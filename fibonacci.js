6aaaa8d92b41520d70edfd444218bf9d9316d36b
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-05-17 10:32:30 +0530

    feat: recursive and iterative approach

---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+//what is fibonacci?
+/*
+//recursive approach
+* 0 1 1 2 3 5 8 11 .....
+F(n)=F(n-1)+F(n-2)
+0 => F(0)=>0
+1 => F(1)=>1
+2 => F(2)=F(2-1)+F(2-2)=>F(1)+F(0)=>1+0=>1
+3 => F(3)=F(3-1)+F(3-2)=>F(2)+f(1)=>1+1=>2
+4 => F(4)=F(4-1)+F(4-2)=>F(3)+F(2)=>2+1=>3
+5 => F(5)=F(5-1)+F(5-2)=>F(4)+F(3)=>3+2=>5
+//iterative approach
+Inorder to get the next value in the sequence, we have add last two elements in the sequence
+
+*/
